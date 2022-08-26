eb2585870bdfea8b5626f4640d640420b18db7b0
George Popovic <georgesmpopovic@gmail.com>
2022-08-26 00:34:10 -0400

    monitor vehicles bustime request

--- app/Mta/BusTime/BusTime.php
+++ app/Mta/BusTime/BusTime.php
@@ -2,9 +2,15 @@
 
 namespace App\Mta\BusTime;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
+
+
 class BusTime {
 
     private $api_key = "";
+    private $response = [];
+    private $response_body = [];
 
     public function __construct(){
 
@@ -16,12 +22,73 @@ class BusTime {
     private function setUp(){
         
         $this->setApiKey();
-        
+
     }
 
     private function setApiKey(){
 
         $this->api_key = config("mta.bustime.credentials.developer_key");
+
+    }
+
+    public function monitorVehicles(){
+
+        $client = new Client();
+        
+        $url = $this->siriVehicleMonitoringUrl();
+
+        $request = new Request('GET', $url);
+        
+        $query_params = $this->queryParams();
+
+        $response = $client->send($request, $query_params);
+
+        $this->captureResponse($response);
+
+    }
+
+    public function captureResponse($response = new \Psr\Http\Message\ResponseInterface()){
+
+        if(isset($response) && isset($response->getBody())){
+
+            $this->response = $response; 
+
+            $this->response_body = json_decode($response->getBody(), true);
+        
+        }
+
+    }
+
+    public function siriVehicleMonitoringUrl(){
+
+        $url = config("mta.bustime.api.siri_base_uri") . config("mta.bustime.api.siri_endpoints.vehicle_monitoring");
+
+        return $url; 
+
+    }
+
+    public function queryParams($params = []){
+
+        $query_params = [];
+        $query_params['query'] = []; 
+
+        if(isset($params) && !empty($params)){
+
+            foreach($params as $k => $v){
+
+                $query_params['query'][$k] = $v;         
+            
+            }
+        
+        }
+
+        $query_params['query']['key'] = $this->api_key;
+
+        return $query_params; 
+
+    }
+
+    public function monitorStops(){
 
     }
 
