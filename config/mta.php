5d58a2fba70a5f02c05ce2a46f2b9ea03e4aa9db
George Popovic <georgesmpopovic@gmail.com>
2022-08-25 23:55:29 -0400

    mta config file

--- config/mta.php
+++ config/mta.php
@@ -1,33 +1,37 @@
-<?php 
-
+<?php
 
 return [
+    "bustime" => [
 
-    // https://bustime.mta.info/wiki/Developers/SIRIIntro
+        "credentials" => [
+            "developer_key" => env("MTA_BUSTIME_API_KEY", ""),
+        ],
 
-    "siri_base_uri" => "http://bustime.mta.info/api/siri/",
+        "api" => [
+            // https://bustime.mta.info/wiki/Developers/SIRIIntro
 
-    "siri_endpoints" => [
-        "vehicle_monitoring" => "vehicle-monitoring.json",
-        "stop_monitoring" => "stop-monitoring.json",
+            "siri_base_uri" => "http://bustime.mta.info/api/siri/",
+
+            "siri_endpoints" => [
+                "vehicle_monitoring" => "vehicle-monitoring.json",
+                "stop_monitoring" => "stop-monitoring.json",
+
+            ],
+
+            // https://bustime.mta.info/wiki/Developers/OneBusAwayRESTfulAPI
+            // Incomplete.
+
+            "oba_base_uri" => " http://bustime.mta.info/api/where/",
+
+            "oba_endpoints" => [
+                "agencies" => "agencies-with-coverage.xml",
+                "mta_routes" => "routes-for-agency/MTA%20NYCT.xml",
+                "stop" => "stop/", // stop/MTA_STOP-ID.xml
+                "stops_for_route/" => "stops-for-route/MTA%20NYCT_", //stops-for-route/MTA%20NYCT_M1.json?key=YOUR_KEY_HERE&includePolylines=false&version=2
+                "stops_near_location" => "stops-for-location.json", // ?lat=40.748433&lon=-73.985656&latSpan=0.005&lonSpan=0.005&key=YOUR_KEY_HERE
+
+            ],
+        ],
 
     ],
-
-    // https://bustime.mta.info/wiki/Developers/OneBusAwayRESTfulAPI
-    // Incomplete. 
-
-    "oba_base_uri" => " http://bustime.mta.info/api/where/",
-
-    "oba_endpoints" => [
-        "agencies" => "agencies-with-coverage.xml",
-        "mta_routes" => "routes-for-agency/MTA%20NYCT.xml",
-        "stop" => "stop/" , // stop/MTA_STOP-ID.xml
-        "stops_for_route/" => "stops-for-route/MTA%20NYCT_", //stops-for-route/MTA%20NYCT_M1.json?key=YOUR_KEY_HERE&includePolylines=false&version=2
-        "stops_near_location" => "stops-for-location.json" // ?lat=40.748433&lon=-73.985656&latSpan=0.005&lonSpan=0.005&key=YOUR_KEY_HERE
-
-
-    ]
-
 ];
-
-    ?>
