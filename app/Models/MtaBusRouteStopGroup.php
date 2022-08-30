98c6b60aafdc8aef824281e3878ea1b30d4a4c07
George Popovic <georgesmpopovic@gmail.com>
2022-08-30 16:39:51 -0400

    bus stop group, model and miogration

--- app/Models/MtaBusRouteStopGroup.php
+++ app/Models/MtaBusRouteStopGroup.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class MtaBusRouteStopGroup extends Model
 {
     use HasFactory;
+
+    public $fillable = [
+        "name",
+        "type",
+        "mta_bus_route_id",
+        "line_ref",
+        "stop_ids"
+    ];
+
 }
