37bf490407e57e030c97ec3c641c06af96639b7c
George Popovic <georgesmpopovic@gmail.com>
2022-08-30 12:40:10 -0400

    comment out other seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,6 +9,7 @@ use Database\Seeders\LineSeeder;
 use Database\Seeders\StopSeeder;
 use Database\Seeders\JourneySeeder;
 use Database\Seeders\MonitoredSeeder;
+use Database\Seeders\MtaBusRouteSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -26,17 +27,20 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        $line_seeder = new LineSeeder;
-        $line_seeder->run();
+        // $line_seeder = new LineSeeder;
+        // $line_seeder->run();
 
-        $stop_seeder = new StopSeeder;
-        $stop_seeder->run();
+        // $stop_seeder = new StopSeeder;
+        // $stop_seeder->run();
 
-        $journey_seeder = new JourneySeeder;
-        $journey_seeder->run();
+        // $journey_seeder = new JourneySeeder;
+        // $journey_seeder->run();
 
-        $monitored_call = new MonitoredCallSeeder;
-        $monitored_call->run();
+        // $monitored_call = new MonitoredCallSeeder;
+        // $monitored_call->run();
+
+        $mta_bus_route_seeder = new MtaBusRouteSeeder;
+        $mta_bus_route_seeder->run();
 
     }
 }
